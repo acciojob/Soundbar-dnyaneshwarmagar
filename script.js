2b13f09acd21a2aeeb96df055c178ec28f4fb84e
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-04 04:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(e) {
   const buttons = document.querySelectorAll('.sound-button');
 
   buttons.forEach(button => {
     button.addEventListener('soundstart', function(event) {
-      const soundFile = this.getAttribute('data-sound');
+      const soundFile = e.target.getAttribute('data-sound');
       playSound(soundFile);
     });
   });
