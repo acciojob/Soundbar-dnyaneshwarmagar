cb2ca779d36d27a51e43836201f28b8adab3da8f
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-06 10:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-//your JS code here. If required.
-let currentAudio = null;
+// Wait for the DOM content to be fully loaded before executing the script
+document.addEventListener('DOMContentLoaded', function() {
+  // Select all buttons with the class 'btn'
+  const buttons = document.querySelectorAll('.btn');
+  // Variable to store the audio element
+  let audio = null;
 
-function playSound(soundUrl) {
-    // If there's an audio playing, stop it first
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-    }
+  // Add event listeners to each button
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      // Get the value of the 'data-sound' attribute of the clicked button
+      const soundFile = this.getAttribute('data-sound');
+      // Check if the button has a 'data-sound' attribute
+      if (soundFile) {
+        // If the button has a 'data-sound' attribute, play the corresponding sound
+        playSound(soundFile);
+      } else {
+        // If the button does not have a 'data-sound' attribute, stop the audio
+        stopSound();
+      }
+    });
+  }
 
-    // Use the provided URL for the sound
-    currentAudio = new Audio(soundUrl);
-    currentAudio.play();
-}
-
-function stopSound() {
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-    }
-}
+  // Function to play a sound
+  function playSound(soundFile) {
+    // Stop any currently playing audio
+    stopSound();
+    // Create a new audio element with the specified sound file
+    audio = new Audio(`sounds/${sound}`);
+  }
